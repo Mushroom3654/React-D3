a7c832016540ab310ee2fb0a3c023973d743df11
kby <redsky__7@naver.com>
2019-07-10 23:14:51 +0900

    bar chart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import D3Bar from "./components/d3-bar";
+import StackedBar from "./components/stacked-bar";
+
 
 
 class App extends  Component{
@@ -13,10 +15,15 @@ class App extends  Component{
 
     render() {
         return (
-            <div className="App">
-                <D3Bar data={this.state.data} width={this.state.width}
-                height={this.state.height}/>
-            </div>
+            <Fragment>
+                <div className="App">
+                    <D3Bar data={this.state.data} width={this.state.width}
+                    height={this.state.height}/>
+                </div>
+                <div>
+                    <StackedBar/>
+                </div>
+            </Fragment>
         );
     }
 }
